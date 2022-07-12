b661c0756e07f56e93993df041eb129561a1575c
michaelvaltix <83365005+michaelvaltix@users.noreply.github.com>
2022-07-12 00:16:08 -0700

    update gcp service account

--- valtix-tutorial/discover.tf
+++ valtix-tutorial/discover.tf
@@ -14,10 +14,9 @@ module "csp_setup" {
 resource "valtix_cloud_account" "gcp_demo" {
   name                 = var.valtix_account_name
   csp_type             = "GCP"
-  gcp_credentials_file = file(var.gcp_key_file)
+  gcp_credentials_file = module.csp_setup.private_key_file_content
   inventory_monitoring {
     regions          = [var.region]
     refresh_interval = 60
   }
-   depends_on = [module.csp_setup]
 }
